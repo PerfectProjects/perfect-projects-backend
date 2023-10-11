594eb54ff10ed829b9f447cdedc8b2bbefdc8519
reoskaro434 <reoskaro434@gmail.com>
2023-10-11 17:41:31 +0200

    Disabled registering

--- backend/rest/access_endpoint.py
+++ backend/rest/access_endpoint.py
@@ -8,12 +8,12 @@ from backend.decorators import require_authentication
 access = Blueprint("access", __name__)
 
 
-@access.route('/access/sign-up', methods=["POST"])
-def sign_up_endpoint():
-    decoded_data = request.data.decode()
-    decoded_data = json.loads(decoded_data)
-    new_user = decoded_data.get("newUser")
-    return AccessController().create_account(new_user)
+# @access.route('/access/sign-up', methods=["POST"])
+# def sign_up_endpoint():
+#     decoded_data = request.data.decode()
+#     decoded_data = json.loads(decoded_data)
+#     new_user = decoded_data.get("newUser")
+#     return AccessController().create_account(new_user)
 
 
 @access.route("/access/sign-in", methods=["POST"])
@@ -31,13 +31,13 @@ def refresh_token_endpoint():
     return AccessController().refresh_token(ref_token, username)
 
 
-@access.route('/access/verify-account', methods=["POST"])
-def verify_account_endpoint():
-    decoded_data = request.data.decode()
-    decoded_data = json.loads(decoded_data)
-    username = decoded_data.get("username")
-    confirmation_code = decoded_data.get("confirmationCode")
-    return AccessController().verify_account(username, confirmation_code)
+# @access.route('/access/verify-account', methods=["POST"])
+# def verify_account_endpoint():
+#     decoded_data = request.data.decode()
+#     decoded_data = json.loads(decoded_data)
+#     username = decoded_data.get("username")
+#     confirmation_code = decoded_data.get("confirmationCode")
+#     return AccessController().verify_account(username, confirmation_code)
 
 
 @access.route('/access/sign-out', methods=["POST"])
